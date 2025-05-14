78ed7c53bc85c45d0c5e93161dae1d5d0e8eb556
rodomanovt <timondeseptikon@gmail.com>
2025-05-14 21:05:23 +0300

    Add back button for songs fragment

--- app/src/main/java/com/rodomanovt/freedomplayer/fragments/SongsFragment.kt
+++ app/src/main/java/com/rodomanovt/freedomplayer/fragments/SongsFragment.kt
@@ -11,8 +11,10 @@ import androidx.documentfile.provider.DocumentFile
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.rodomanovt.freedomplayer.R
 import com.rodomanovt.freedomplayer.activities.MainActivity
 import com.rodomanovt.freedomplayer.adapters.SongsAdapter
 import com.rodomanovt.freedomplayer.databinding.FragmentSongsBinding
@@ -34,6 +36,13 @@ class SongsFragment : Fragment() {
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.backBtn.setOnClickListener{
+            findNavController().navigate(R.id.action_songsFragment_to_playlistsFragment)
+        }
+
+        // должно показываться название плейлиста binding.playlistNameText.text =
+
         setupRecyclerView()
         loadSongs()
         setupObservers()
